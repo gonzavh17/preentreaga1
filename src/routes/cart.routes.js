2a33607dc6e300760ffbccc0b2a24ca77461b61c
gonzavh17 <gonzaasis17@gmail.com>
2023-09-13 20:58:31 -0300

    fixing post

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -46,23 +46,29 @@ cartRouter.post("/", async (req, res) => {
 // Agregar producto al carrito
 cartRouter.post("/:cid/products/:pid", async (req, res) => {
   const { cid, pid } = req.params;
-  const { quantity } = req.body;
 
   try {
     const searchCart = await cartModel.findById(cid);
 
     if (searchCart) {
-      searchCart.products.push({ id_prod: pid, quantity: quantity });
+      const existingProductIndex = searchCart.products.findIndex(
+        (product) => product.id_prod.toString() === pid
+      );
+
+      if (existingProductIndex !== -1) {
+        const existingProduct = searchCart.products[existingProductIndex];
+        existingProduct.quantity += 1;
+      } else {
+        searchCart.products.push({ id_prod: pid, quantity: 1 });
+      }
 
       const updatedCart = await searchCart.save();
 
-      res
-        .status(200)
-        .send({
-          result: "OK",
-          message: "Producto agregado exitosamente",
-          cart: updatedCart,
-        });
+      res.status(200).send({
+        result: "OK",
+        message: "Producto agregado exitosamente",
+        cart: updatedCart,
+      });
     } else {
       res.status(404).send({ error: "Carrito no encontrado" });
     }
@@ -72,57 +78,65 @@ cartRouter.post("/:cid/products/:pid", async (req, res) => {
 });
 
 // Update a Cart
-cartRouter.put('/:cid', async (req, res) => {
-  const { cid } = req.params
-  const { products } = req.body
+cartRouter.put("/:cid", async (req, res) => {
+  const { cid } = req.params;
+  const { products } = req.body;
 
   try {
-    const searchCart = await cartModel.findById(cid)
+    const searchCart = await cartModel.findById(cid);
 
     if (searchCart) {
-      searchCart.products = products
+      searchCart.products = products;
 
-      const updatedCart = await searchCart.save()
+      const updatedCart = await searchCart.save();
 
-      res.status(200).send({ result: 'OK', message: 'Cart updated', cart: updatedCart })
+      res
+        .status(200)
+        .send({ result: "OK", message: "Cart updated", cart: updatedCart });
     } else {
-      res.status(404).send({ error: 'Cart Nout Found' })
+      res.status(404).send({ error: "Cart Nout Found" });
     }
+  } catch (error) {
+    res.status(400).send({ error: `Error updating the cart: ${error}` });
   }
+});
 
-  catch (error) {
-    res.status(400).send({ error: `Error updating the cart: ${error}` })
-  }
-})
-
-cartRouter.put('/:cid/products/:pid', async (req, res) => {
-  const { cid, pid } = req.params
-  const { quantity } = req.body
+cartRouter.put("/:cid/products/:pid", async (req, res) => {
+  const { cid, pid } = req.params;
+  const { quantity } = req.body;
 
   try {
-    const searchCart = await cartModel.findById(cid)
+    const searchCart = await cartModel.findById(cid);
 
     if (searchCart) {
-      const productIndex = searchCart.products.findIndex(product => product.id_prod.toString() === pid)
+      const productIndex = searchCart.products.findIndex(
+        (product) => product.id_prod.toString() === pid
+      );
 
       if (productIndex !== -1) {
-        searchCart.products[productIndex].quantity = quantity
+        searchCart.products[productIndex].quantity = quantity;
 
-        const updatedCart = await searchCart.save()
+        const updatedCart = await searchCart.save();
 
-        res.status(200).send({ result: 'OK', message: 'Cantidad del producto', cart: updatedCart })
+        res
+          .status(200)
+          .send({
+            result: "OK",
+            message: "Cantidad del producto",
+            cart: updatedCart,
+          });
       } else {
-        res.status(404).send({ error: 'Producto no encontrado en el carrito' })
+        res.status(404).send({ error: "Producto no encontrado en el carrito" });
       }
     } else {
-      res.status(404).send({ error: 'Carrito no encontrado' })
+      res.status(404).send({ error: "Carrito no encontrado" });
     }
+  } catch (error) {
+    res
+      .status(400)
+      .send({ error: `Error al actualizar producto del carrito: ${error}` });
   }
-
-  catch (error) {
-    res.status(400).send({ error: `Error al actualizar producto del carrito: ${error}` })
-  }
-})
+});
 
 cartRouter.delete("/:cid", async (req, res) => {
   const { cid } = req.params;
@@ -135,8 +149,6 @@ cartRouter.delete("/:cid", async (req, res) => {
     res.status(400).send({ error: `Error al eliminar carritos: ${error}` });
   }
 });
-
-
 
 export default cartRouter;
 
